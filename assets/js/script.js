c33bab3b108ca327d94ab040e900ba7d2c8f42fb
FenrirKin <jarfjinq@students.codeinstitute.net>
2026-03-19 15:10:00 +0000

    "Add multiplication game code"

--- assets/js/script.js
+++ assets/js/script.js
@@ -30,7 +30,11 @@ function runGame(gameType) {
 
         if (gameType === "addition") {
                 displayAdditionQuestion(num1, num2);
-        } else {
+        } else if (gameType === "multiplication") {
+                displayMultiplyQuestion(num1, num2);
+        }
+        
+        else {
                 alert(`Unknown game type: ${gameType}`);
                 throw`Unknown game type: ${gameType}. Aborting!`; 
         }
@@ -69,6 +73,8 @@ function calculateCorrectAnswer() {
 
         if (operator === "+") {
                 return [operand1 + operand2, "addition"];
+        } else if (operator === "x") {
+                return [operand1 * operand2, "multiplication"];
         } else {
                 alert(`Unimplemented operator ${operator}`);
                 throw `Unimplemented operator ${operator}. Aborting!`;
@@ -104,7 +110,10 @@ function displaySubtractQuestion() {
 
 }
 
-function displayMultiplyQuestion() {
+function displayMultiplyQuestion(operand1, operand2) {
+        document.getElementById(`operand1`).textContent = operand1;
+        document.getElementById(`operand2`).textContent = operand2;
+        document.getElementById(`operator`).textContent = "x";
 
 }
 
