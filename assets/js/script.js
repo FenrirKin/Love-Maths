b7babe6cfd9b180e96b62216dad690fd8a57e7d5
FenrirKin <jarfjinq@students.codeinstitute.net>
2026-03-19 15:20:04 +0000

    "Add code for subtraction game"

--- assets/js/script.js
+++ assets/js/script.js
@@ -32,9 +32,11 @@ function runGame(gameType) {
                 displayAdditionQuestion(num1, num2);
         } else if (gameType === "multiplication") {
                 displayMultiplyQuestion(num1, num2);
-        }
-        
-        else {
+        } else if (gameType === "subtraction") {
+                displaySubtractQuestion(num1, num2);
+        } else if (gameType === "division") {
+                displayDivideQuestion(num1, num2);
+        } else {
                 alert(`Unknown game type: ${gameType}`);
                 throw`Unknown game type: ${gameType}. Aborting!`; 
         }
@@ -75,7 +77,10 @@ function calculateCorrectAnswer() {
                 return [operand1 + operand2, "addition"];
         } else if (operator === "x") {
                 return [operand1 * operand2, "multiplication"];
-        } else {
+        } else if (operator === "-") {
+                return [operand1 - operand2, "subtraction"];
+        }
+        else {
                 alert(`Unimplemented operator ${operator}`);
                 throw `Unimplemented operator ${operator}. Aborting!`;
         }
@@ -106,7 +111,10 @@ function displayAdditionQuestion(operand1, operand2) {
 
 }
 
-function displaySubtractQuestion() {
+function displaySubtractQuestion(operand1, operand2) {
+        document.getElementById("operand1").textContent = operand1 > operand2 ? operand1 : operand2;
+        document.getElementById("operand2").textContent = operand1 > operand2 ? operand2 : operand1;
+        document.getElementById(`operator`).textContent = "-";
 
 }
 
